de9f8240939bced1785c3c9341bc4870d15ce6a3
KRUTOMER <94799758+KRUTOMER@users.noreply.github.com>
2022-01-24 18:49:01 +0300

    Update UART.v

--- UART.v
+++ UART.v
@@ -7,11 +7,11 @@ module UART_receiver
     input wire clk, //reset,
     input wire DATA_serial,
     
-//    output reg data_bit = 0, 
-  //  output reg data_bit2 = 0,
-   // output reg [2:0] STATE,
-   // output reg [11:0] clk_counter,
- //   output reg [2:0] bit_index,
+   // output reg data_bit = 0, 
+   // output reg data_bit2 = 0,
+    //output reg [2:0] STATE_reg = 0,
+   //output reg [11:0] clk_counter_reg = 0,
+   //output reg [2:0] bit_index_reg = 0,
     
     output reg done_tick = 0,
     output wire [7:0] DATA_byte
@@ -19,98 +19,103 @@ module UART_receiver
     );
     
 //signal declaration
-reg [11:0] clk_counter = 0;
+reg [11:0] clk_counter_reg = 0;
+reg [11:0] clk_counter_next;
 reg data_bit, data_bit2;
-reg [2:0] bit_index = 0;
+reg [2:0] bit_index_reg = 0;
+reg [2:0] bit_index_next;
 reg [7:0] byte_bit = 0;
-reg [2:0] STATE = 0;
+reg [2:0] STATE_reg = 0;
+reg [2:0] STATE_next;
 
 //dowble register body
 always @(posedge clk)
   begin
     data_bit2 <= DATA_serial;
     data_bit <= data_bit2;
+    STATE_reg <= STATE_next;
+    clk_counter_reg <= clk_counter_next;
+    bit_index_reg <= bit_index_next;
   end
-
-    
-
 
 //state declaration
 localparam [2:0] IDLE = 3'b000,
                  START_BIT = 3'b001,
                  DATA_TRANSFER = 3'b010,
                  STOP_BIT = 3'b011,
-                 CLEAN_UP = 3'b100;
+                 DONE = 3'b100;
                  
 //------------FSM BODY---------------
-always @(posedge clk)
+always @*
   begin
-    case (STATE) IDLE: begin
-                       //waiting start_bit
-                       done_tick <= 0;
-                       clk_counter <= 0;
-                       bit_index <= 0;
-                         if (data_bit == 0) 
-                           STATE <= START_BIT;
-                         else
-                           STATE <= IDLE;
-                       end
+    done_tick = 0;
+    clk_counter_next = clk_counter_reg;
+    bit_index_next = bit_index_reg;
+    STATE_next = STATE_reg;
+  
+  
+      case (STATE_reg) IDLE: begin
+                           //waiting start_bit
+                               if (data_bit == 0) 
+                                 STATE_next = START_BIT;
+                               else
+                                 STATE_next = IDLE;
+                             end
                        
            START_BIT: begin
                       //check the midle of start_bit
-                      done_tick <= 0;
-                      clk_counter <= 0;
-                      bit_index <= 0;
-                        if (clk_counter == ((FREQ/2/BAUD_RATE)-1)) 
+                        if (clk_counter_next == ((FREQ/2/BAUD_RATE)-1)) 
                           if (data_bit == 0) begin 
-                            clk_counter <= 0; //midle of input_bit duration
-                            STATE <= DATA_TRANSFER; end
+                            clk_counter_next = 0; //midle of input_bit duration
+                            STATE_next = DATA_TRANSFER; end
                           else 
-                            STATE <= IDLE; 
-                        
+                            STATE_next = IDLE; 
                         else begin
-                          clk_counter <= clk_counter + 1;
-                          STATE <= START_BIT;
+                          clk_counter_next = clk_counter_reg + 1;
+                          STATE_next = START_BIT;
                         end
                       end
                       
        DATA_TRANSFER: begin
                       // receive 8 bit of data
-                      done_tick <= 0;
-                      //bit_index <= 0;
-                        if (clk_counter < ((FREQ/BAUD_RATE)-1)) begin
-                          clk_counter <= clk_counter + 1;  
-                          STATE <= DATA_TRANSFER; end
+                        if (clk_counter_next < ((FREQ/BAUD_RATE)-1)) begin
+                          clk_counter_next = clk_counter_reg + 1;  
+                          STATE_next = DATA_TRANSFER; end
                         else begin
-                          clk_counter <= 0;
-                          byte_bit[bit_index] <= data_bit;
-                            if (bit_index < 7) begin
-                              bit_index <= bit_index + 1;
-                              STATE <= DATA_TRANSFER; end
+                          clk_counter_next = 0;
+                          byte_bit[bit_index_next] = data_bit;
+                            if (bit_index_next < 7) begin
+                              bit_index_next = bit_index_reg + 1;
+                              STATE_next = DATA_TRANSFER; end
                             else begin
-                              bit_index <= 0;
-                              STATE <= STOP_BIT; end
+                              bit_index_next = 0;
+                              STATE_next = STOP_BIT; end
                         end
                      end
                      
            STOP_BIT: begin
                      // receive stop-bit
-                       if (clk_counter < ((FREQ/BAUD_RATE)-1)) begin
-                         clk_counter <= clk_counter + 1;
-                         STATE <= STOP_BIT; end
+                       if (clk_counter_next < ((FREQ/BAUD_RATE)-1)) begin
+                         clk_counter_next = clk_counter_reg + 1;
+                         STATE_next = STOP_BIT; end
                        else begin
-                         done_tick <= 1;
-                         clk_counter <= 0;
-                         STATE <= CLEAN_UP; end
+                        // done_tick = 1;
+                         clk_counter_next = 0;
+                         STATE_next = DONE; end
                      end
                      
-           CLEAN_UP: begin
+           DONE: begin
                      //waiting 1 clk cycle
-                     done_tick <= 0;
-                     STATE <= IDLE;
+                     done_tick = 1;
+                     STATE_next = IDLE;
                      end
                      
-             default: STATE <= IDLE;
+             default: STATE_next = IDLE;
   endcase
 end
    
+   
+assign DATA_byte = byte_bit;   
+                                            
+    
+endmodule
